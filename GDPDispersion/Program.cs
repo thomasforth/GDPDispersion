6b219e2036ebf3d7a8da76c1df4e09fecac8bac6
Thomas Forth <thomas.forth@gmail.com>
2019-11-28 13:04:23 +0000

    Fixes and simplification

--- GDPDispersion/Program.cs
+++ GDPDispersion/Program.cs
@@ -17,15 +17,6 @@ namespace GDPDispersion
             List<GDPPerHeadRaw> RawGDPPerHead;
             List<IncomeRaw> RawIncomes;
 
-            // Load the three files
-            /*
-            using (TextReader textReader = File.OpenText(@"Assets\nama_10r_2gdp_1_Data.csv"))
-            {
-                CsvReader csvReader = new CsvReader(textReader);
-                RawGDPTotals = new List<GDPTotalRaw>(csvReader.GetRecords<GDPTotalRaw>());
-            }
-            */
-
             using (TextReader textReader = File.OpenText(@"Assets\nama_10r_2gdp_2_Data.csv"))
             {
                 CsvReader csvReader = new CsvReader(textReader);
@@ -135,30 +126,23 @@ namespace GDPDispersion
             List<DispersionOutput> CurrentDispersionOutputs = CalculateDispersions(CombinedCurrentData);
 
 
-            // Merge the two outputs (prefering the current dataset results)
-            List<DispersionOutput> CombinedDispersionOutputs = new List<DispersionOutput>();
+            // Merge the two outputs (prefering the dataset which was calculated using the highest number of regions)
+            List<DispersionOutput> CombinedDispersionOutputs = new List<DispersionOutput>();            
             CombinedDispersionOutputs.AddRange(HistoricDispersionOutputs);
-            foreach(DispersionOutput currentDispersionOutput in CurrentDispersionOutputs)
+            foreach (DispersionOutput currentDispersionOutput in CurrentDispersionOutputs)
             {
-                if (currentDispersionOutput.Country.StartsWith("EU") || currentDispersionOutput.Country.StartsWith("EA"))
+                if (CombinedDispersionOutputs.Exists(x => x.Country == currentDispersionOutput.Country && x.Year == currentDispersionOutput.Year) == false)
                 {
-                    if (currentDispersionOutput.Year >= 2015)
-                    {
-                        CombinedDispersionOutputs.RemoveAll(x => x.Country == currentDispersionOutput.Country && x.Year == currentDispersionOutput.Year);
-                        CombinedDispersionOutputs.Add(currentDispersionOutput);
-                    }
+                    CombinedDispersionOutputs.Add(currentDispersionOutput);
+                }
+                else if (CombinedDispersionOutputs.Exists(x => x.Country == currentDispersionOutput.Country && x.Year == currentDispersionOutput.Year && x.RegionsConsidered < currentDispersionOutput.RegionsConsidered))
+                {
+                    CombinedDispersionOutputs.RemoveAll(x => x.Country == currentDispersionOutput.Country && x.Year == currentDispersionOutput.Year);
+                    CombinedDispersionOutputs.Add(currentDispersionOutput);
                 }
                 else
                 {
-                    if (double.IsNaN(currentDispersionOutput.GDPDispersion) == false && CombinedDispersionOutputs.Exists(x => x.Country == currentDispersionOutput.Country && x.Year == currentDispersionOutput.Year))
-                    {
-                        CombinedDispersionOutputs.RemoveAll(x => x.Country == currentDispersionOutput.Country && x.Year == currentDispersionOutput.Year);
-                        CombinedDispersionOutputs.Add(currentDispersionOutput);
-                    }
-                    if (CombinedDispersionOutputs.Exists(x => x.Country == currentDispersionOutput.Country && x.Year == currentDispersionOutput.Year) == false)
-                    {
-                        CombinedDispersionOutputs.Add(currentDispersionOutput);
-                    }
+                    // the existing calculated dispersion was calculated from a higher number of regions than the potential replacement. The existing dispersion is retained.
                 }
             }
 
@@ -175,9 +159,6 @@ namespace GDPDispersion
             // All the data we need is now loaded in. Time to calculate Theil Index and GDP dispersion
             // https://en.wikipedia.org/wiki/Theil_index
             // 
-
-            List<DispersionOutput> DispersionOutputs = new List<DispersionOutput>();
-
             List<string> UniqueCountries = CombinedData.Select(x => x.RegionCode.Substring(0, 2)).Distinct().ToList();
             UniqueCountries.Remove("EU");
             UniqueCountries.Add("EA12");
@@ -185,11 +166,14 @@ namespace GDPDispersion
             UniqueCountries.Add("EU19");
             UniqueCountries.Add("EU27");
             UniqueCountries.Add("UK*");
+            UniqueCountries.Add("Nordics");
+            UniqueCountries.Add("North England");
+            UniqueCountries.Add("Low Countries");
 
             List<int> UniqueYears = CombinedData.Select(x => x.Year).Distinct().ToList();
+            List<DispersionOutput> DispersionOutputs = new List<DispersionOutput>();
             foreach (string country in UniqueCountries)
             {
-                string loopcountry = country;
                 List<CombinedData> NUTS2RegionsInTheCountry = CombinedData.Where(x => x.RegionCode.StartsWith(country) && x.NUTSLevel == 2).ToList();
 
                 // whole-EU calculations are a special case. The LondonFix is applied and all NUTS2 regions for nations within the EU are used.
@@ -198,12 +182,42 @@ namespace GDPDispersion
                 List<string> EU15Countries = new List<string>() { "AT", "BE", "DE", "DK", "EL", "ES", "FI", "FR", "IE", "IT", "LU", "NL", "PT", "SE", "UK" };
                 List<string> EU19Countries = new List<string>() { "AT", "BE", "DE", "DK", "EL", "ES", "FI", "FR", "IE", "IT", "LU", "NL", "PT", "SE", "UK", "SK", "PL", "HU", "CZ" };
                 List<string> EU27Countries = new List<string>() { "AT", "BE", "BG", "CY", "CZ", "DE", "DK", "EE", "EL", "ES", "FI", "FR", "HU", "IE", "IT", "LT", "LU", "LV", "MT", "NL", "PL", "PT", "RO", "SE", "SI", "SK", "UK" };
+                List<string> Nordics = new List<string>() { "SE", "FI", "DK" };
+                List<string> NorthEngland = new List<string>() { "UKE", "UKD", "UKC" };
+                List<string> LowCountries = new List<string>() { "NL", "BE" };                
 
                 // IRELAND is REMOVED BECAUSE ITS GDP DATA IS USELESS
                 EA12Countries.Remove("IE");
                 EU15Countries.Remove("IE");
                 EU19Countries.Remove("IE");
                 EU27Countries.Remove("IE");
+
+                if (country == "Nordics")
+                {
+                    NUTS2RegionsInTheCountry.Clear();
+                    foreach (string EUcountry in Nordics)
+                    {
+                        NUTS2RegionsInTheCountry.AddRange(CombinedData.Where(x => x.RegionCode.StartsWith(EUcountry) && x.NUTSLevel == 2));
+                    }
+                }
+
+                if (country == "North England")
+                {
+                    NUTS2RegionsInTheCountry.Clear();
+                    foreach (string EUcountry in NorthEngland)
+                    {
+                        NUTS2RegionsInTheCountry.AddRange(CombinedData.Where(x => x.RegionCode.StartsWith(EUcountry) && x.NUTSLevel == 2));
+                    }
+                }
+
+                if (country == "Low Countries")
+                {
+                    NUTS2RegionsInTheCountry.Clear();
+                    foreach (string EUcountry in LowCountries)
+                    {
+                        NUTS2RegionsInTheCountry.AddRange(CombinedData.Where(x => x.RegionCode.StartsWith(EUcountry) && x.NUTSLevel == 2));
+                    }
+                }
 
                 if (country == "EA12")
                 {
@@ -216,6 +230,7 @@ namespace GDPDispersion
                     NUTS2RegionsInTheCountry.RemoveAll(x => x.RegionCode.StartsWith("UKI"));
                     NUTS2RegionsInTheCountry.AddRange(CombinedData.Where(x => x.RegionCode == "UKI").ToList());
                 }
+
                 if (country == "EU15")
                 {
                     NUTS2RegionsInTheCountry.Clear();
@@ -255,7 +270,6 @@ namespace GDPDispersion
                     // There are three approaches, London, MegaLondon, and PartialMegaLondon
                     // Of these I think "London" is the best mix of fair and simple.
                     string approach = "LondonFix";
-
                     if (approach == "LondonFix")
                     {
                         // The UK has stupid NUTS regions, so we need to swap out the five NUTS2 regions for London with the NUTS1 region for London
@@ -263,71 +277,6 @@ namespace GDPDispersion
                         NUTS2RegionsInTheCountry.RemoveAll(x => x.RegionCode.StartsWith("UKI"));
                         NUTS2RegionsInTheCountry.AddRange(CombinedData.Where(x => x.RegionCode == "UKI").ToList());
                     }
-
-                    /*
-                    if (approach == "MegaLondon")
-                    {
-                        // We might also want to combine London, East, and South-East into a single mega region
-                        // These are UKI, UKJ, UKH.
-                        // First we remove all NUTS regions in those regions
-                        NUTS2RegionsInTheCountry.RemoveAll(x => x.RegionCode.StartsWith("UKJ") || x.RegionCode.StartsWith("UKH") || x.RegionCode.StartsWith("UKI"));
-
-                        // Then we create and add mega london
-                        foreach (int year in UniqueYears)
-                        {
-                            CombinedData UKI = Combined.Where(x => x.RegionCode == "UKI" && x.Year == year).First();
-                            CombinedData UKJ = Combined.Where(x => x.RegionCode == "UKJ" && x.Year == year).First();
-                            CombinedData UKH = Combined.Where(x => x.RegionCode == "UKH" && x.Year == year).First();
-
-                            CombinedData MegaLondon = new CombinedData();
-                            MegaLondon.RegionCode = "UKZ";
-                            MegaLondon.GDPTotal = UKI.GDPTotal + UKJ.GDPTotal + UKH.GDPTotal;
-                            MegaLondon.IncomePerHead = Convert.ToInt32((UKI.IncomePerHead * UKI.Population + UKJ.IncomePerHead * UKJ.Population + UKH.IncomePerHead * UKH.Population) / (UKI.Population + UKJ.Population + UKH.Population));
-                            MegaLondon.GDPPerHead = Convert.ToInt32((UKI.GDPPerHead * UKI.Population + UKJ.GDPPerHead * UKJ.Population + UKH.GDPPerHead * UKH.Population) / (UKI.Population + UKJ.Population + UKH.Population));
-                            MegaLondon.Year = year;
-                            MegaLondon.Population = UKI.Population + UKJ.Population + UKH.Population;
-
-                            NUTS2RegionsInTheCountry.Add(MegaLondon);
-                        }
-                    }
-
-                    // How about tweaking MegaLondon by removing Hampshire UKJ3 (and keeping UKJ1, UKJ2, UKJ4) and removing East Anglia UKH1 (and keeping UKH2, UKH3)?
-                    if (approach == "PartialMegaLondon")
-                    {
-                        // First we remove all NUTS regions in the South-East regions
-                        NUTS2RegionsInTheCountry.RemoveAll(x => x.RegionCode.StartsWith("UKJ") || x.RegionCode.StartsWith("UKH") || x.RegionCode.StartsWith("UKI"));
-                        // Then add back in UKJ3 and UKH1
-
-                        NUTS2RegionsInTheCountry.AddRange(Combined.Where(x => x.RegionCode == "UKJ3").ToList());
-                        NUTS2RegionsInTheCountry.AddRange(Combined.Where(x => x.RegionCode == "UKH1").ToList());
-
-                        //Now create partial mega London
-                        List<string> PartialMegaLondonCodes = new List<string>() { "UKI", "UKJ1", "UKJ2", "UKJ4", "UKH2", "UKH3" };
-                        foreach (int year in UniqueYears)
-                        {
-                            double GDPTotal = 0;
-                            double IncomeTotal = 0;
-                            double PopulationTotal = 0;
-
-                            foreach (string regioncode in PartialMegaLondonCodes)
-                            {
-                                CombinedData region = Combined.Where(x => x.RegionCode == regioncode && x.Year == year).First();
-                                GDPTotal += region.GDPTotal;
-                                IncomeTotal += (region.IncomePerHead * region.Population);
-                                PopulationTotal += region.Population;
-                            }
-
-                            CombinedData PartialMegaLondon = new CombinedData();
-                            PartialMegaLondon.RegionCode = "UKY";
-                            PartialMegaLondon.Year = year;
-                            PartialMegaLondon.IncomePerHead = Convert.ToInt32(IncomeTotal / PopulationTotal);
-                            PartialMegaLondon.Population = PopulationTotal;
-                            PartialMegaLondon.GDPTotal = GDPTotal;
-                            PartialMegaLondon.GDPPerHead = Convert.ToInt32(GDPTotal / PopulationTotal);
-                            NUTS2RegionsInTheCountry.Add(PartialMegaLondon);
-                        }
-                    }
-                    */
                 }
 
                 foreach (int year in UniqueYears)
@@ -337,7 +286,7 @@ namespace GDPDispersion
                     DispersionOutput dispersionOutput = new DispersionOutput()
                     {
                         Year = year,
-                        Country = loopcountry
+                        Country = country
                     };
 
                     dispersionOutput.GDPDispersion = calculateGDPDispersion(NUTS2RegionsInTheCountryThisYear);
@@ -346,7 +295,6 @@ namespace GDPDispersion
 
                     if (CombinedData.Where(x => x.Year == year && x.RegionCode == country).FirstOrDefault() != null)
                     {
-                        //dispersionOutput.IncomeDispersion = calculateIncomeDispersion(NUTS2RegionsInTheCountryThisYear, Combined.Where(x => x.Year == year && x.RegionCode == country).FirstOrDefault().IncomePerHead);
                         dispersionOutput.IncomeDispersion = calculateIncomeDispersion(NUTS2RegionsInTheCountryThisYear);
                         dispersionOutput.IncomeTheilT = calculateIncomeTheilTIndex(NUTS2RegionsInTheCountryThisYear);
                     }
@@ -370,6 +318,14 @@ namespace GDPDispersion
                 if (region.Population == 0)
                 {
                     Console.WriteLine($"Region {region.CountryOrGrouping} has zero population during GDP dispersion calculation. This shouldn't happen!");
+                }
+                if (region.GDPPerHead == 0)
+                {
+                    Console.WriteLine($"Region {region.CountryOrGrouping} has zero GDP per head during GDP dispersion calculation. This shouldn't happen!");
+                }
+                if (region.GDPTotal == 0)
+                {
+                    Console.WriteLine($"Region {region.CountryOrGrouping} has zero total GDP during GDP dispersion calculation. This shouldn't happen!");
                 }
                 D += (Math.Abs(region.GDPPerHead - Y)) * (region.Population / P);
             }
@@ -518,4 +474,70 @@ namespace GDPDispersion
         public string NA_ITEM { get; set; }
         public string Value { get; set; }
     }
+
+
+    /*
+    if (approach == "MegaLondon")
+    {
+        // We might also want to combine London, East, and South-East into a single mega region
+        // These are UKI, UKJ, UKH.
+        // First we remove all NUTS regions in those regions
+        NUTS2RegionsInTheCountry.RemoveAll(x => x.RegionCode.StartsWith("UKJ") || x.RegionCode.StartsWith("UKH") || x.RegionCode.StartsWith("UKI"));
+
+        // Then we create and add mega london
+        foreach (int year in UniqueYears)
+        {
+            CombinedData UKI = Combined.Where(x => x.RegionCode == "UKI" && x.Year == year).First();
+            CombinedData UKJ = Combined.Where(x => x.RegionCode == "UKJ" && x.Year == year).First();
+            CombinedData UKH = Combined.Where(x => x.RegionCode == "UKH" && x.Year == year).First();
+
+            CombinedData MegaLondon = new CombinedData();
+            MegaLondon.RegionCode = "UKZ";
+            MegaLondon.GDPTotal = UKI.GDPTotal + UKJ.GDPTotal + UKH.GDPTotal;
+            MegaLondon.IncomePerHead = Convert.ToInt32((UKI.IncomePerHead * UKI.Population + UKJ.IncomePerHead * UKJ.Population + UKH.IncomePerHead * UKH.Population) / (UKI.Population + UKJ.Population + UKH.Population));
+            MegaLondon.GDPPerHead = Convert.ToInt32((UKI.GDPPerHead * UKI.Population + UKJ.GDPPerHead * UKJ.Population + UKH.GDPPerHead * UKH.Population) / (UKI.Population + UKJ.Population + UKH.Population));
+            MegaLondon.Year = year;
+            MegaLondon.Population = UKI.Population + UKJ.Population + UKH.Population;
+
+            NUTS2RegionsInTheCountry.Add(MegaLondon);
+        }
+    }
+
+    // How about tweaking MegaLondon by removing Hampshire UKJ3 (and keeping UKJ1, UKJ2, UKJ4) and removing East Anglia UKH1 (and keeping UKH2, UKH3)?
+    if (approach == "PartialMegaLondon")
+    {
+        // First we remove all NUTS regions in the South-East regions
+        NUTS2RegionsInTheCountry.RemoveAll(x => x.RegionCode.StartsWith("UKJ") || x.RegionCode.StartsWith("UKH") || x.RegionCode.StartsWith("UKI"));
+        // Then add back in UKJ3 and UKH1
+
+        NUTS2RegionsInTheCountry.AddRange(Combined.Where(x => x.RegionCode == "UKJ3").ToList());
+        NUTS2RegionsInTheCountry.AddRange(Combined.Where(x => x.RegionCode == "UKH1").ToList());
+
+        //Now create partial mega London
+        List<string> PartialMegaLondonCodes = new List<string>() { "UKI", "UKJ1", "UKJ2", "UKJ4", "UKH2", "UKH3" };
+        foreach (int year in UniqueYears)
+        {
+            double GDPTotal = 0;
+            double IncomeTotal = 0;
+            double PopulationTotal = 0;
+
+            foreach (string regioncode in PartialMegaLondonCodes)
+            {
+                CombinedData region = Combined.Where(x => x.RegionCode == regioncode && x.Year == year).First();
+                GDPTotal += region.GDPTotal;
+                IncomeTotal += (region.IncomePerHead * region.Population);
+                PopulationTotal += region.Population;
+            }
+
+            CombinedData PartialMegaLondon = new CombinedData();
+            PartialMegaLondon.RegionCode = "UKY";
+            PartialMegaLondon.Year = year;
+            PartialMegaLondon.IncomePerHead = Convert.ToInt32(IncomeTotal / PopulationTotal);
+            PartialMegaLondon.Population = PopulationTotal;
+            PartialMegaLondon.GDPTotal = GDPTotal;
+            PartialMegaLondon.GDPPerHead = Convert.ToInt32(GDPTotal / PopulationTotal);
+            NUTS2RegionsInTheCountry.Add(PartialMegaLondon);
+        }
+    }
+    */
 }
